430e29b70cae938023d6b5c3bf64ca1eda2cbf34
yasminlobo <125031166+yasminlobo@users.noreply.github.com>
2023-05-26 08:01:28 -0300

    oii

--- SITE-MODELO/main.js
+++ SITE-MODELO/main.js
@@ -1,7 +1,19 @@
-var qtd_itens = document.getElementById("quantity").value;
+    // Adiciona um evento de clique aos botões de compra
+    var buyButtons = document.querySelectorAll('.buy');
+    buyButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        var quantityInput = this.previousElementSibling;
+        var quantity = parseInt(quantityInput.value);
+        var name = this.parentNode.querySelector('.name').innerText;
+        alert('Você comprou ' + quantity + ' unidades do produto: ' + name);
+      });
+    });
 
-
-function deletarItem(){
-    qtd_itens = (qtd_itens)-1;
-    console.log(qtd_itens);
-}
+    // Adiciona um evento de clique aos botões de exclusão
+    var deleteButtons = document.querySelectorAll('.delete');
+    deleteButtons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        var quantityInput = this.parentNode.querySelector('.quantity');
+        quantityInput.value = 0;
+      });
+    });
