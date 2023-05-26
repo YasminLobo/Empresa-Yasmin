59407dcee396b3bc9787b683dec6d9be92a74692
yasminlobo <125031166+yasminlobo@users.noreply.github.com>
2023-05-26 11:37:21 -0300

    ultimo

--- SITE-MODELO/main.js
+++ SITE-MODELO/main.js
@@ -1,14 +1,37 @@
-var counterVal = 0;
+var quantidadeCliques = 0;
+var idQtdItem = document.getElementById("registro-cliques");
+var contar = document.getElementById("preco");
+var prQt = 40;
 
-function incrementClick() {
-    updateDisplay(++counterVal);
+function ContarCliques() {
+    quantidadeCliques++;
+    idQtdItem.innerHTML = `<td>${quantidadeCliques}</td>`;
+    let valor = quantidadeCliques * prQt;
+    preco.innerHTML = `<td>R$${valor},00</td>`;
 }
 
 function resetCounter() {
-    counterVal = 0;
-    updateDisplay(counterVal);
+    quantidadeCliques = 0;
+    idQtdItem.innerHTML = 0;
+   preco.innerHTML = `<td>R$ ${valor},00</td>`;
 }
 
-function updateDisplay(val) {
-    document.getElementById("counter-label").innerHTML = val;
+function excluir() {
+    quantidadeCliques--;
+    idQtdItem.innerHTML = `<td>${quantidadeCliques}</td>`;
+    let valor = quantidadeCliques * prQt;
+    preco.innerHTML = `<td>R$${valor},00</td>`;
 }
+
+function excluir() {
+    if(quantidadeCliques>0) {
+        quantidadeCliques--;
+        idQtdItem.innerHTML = `<td>${quantidadeCliques}</td>`;
+    let valor = quantidadeCliques * prQt;
+    preco.innerHTML = `<td>R$${valor}00,</td>`;
+    }else{
+       alert("Impossível selecionar esse valor");
+    }
+}
+  
+  
