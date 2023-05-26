9aa224c3464cfd1940861befef04114890ae8f66
yasminlobo <125031166+yasminlobo@users.noreply.github.com>
2023-05-26 08:13:36 -0300

    oi

---
+++ home.js
@@ -0,0 +1,30 @@
+// var number = 1;
+// var numberElement = document.getElementById('quantity');
+
+// document.getElementById('buy-button').addEventListener('click', function() {
+//   number++;
+//   updateQuantity();
+//   showPurchaseMessage();
+// });
+
+// document.getElementById('delete-button').addEventListener('click', function() {
+//     console.log("teste");
+//   if (number> 0) {
+//     number--;
+//     updateQuantity();
+//   }
+// });
+
+// function updateQuantity() {
+//   numberElement.textConte
+//   nt = number;
+// }
+
+// function showPurchaseMessage() {
+//   alert("Compra realizada!");
+// }
+
+function deletarItem(){
+    console.log("teste");
+    alert("teste");
+}
